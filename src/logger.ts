bca496951be2a6dc56d3209fce776e6c9641f7a5
Audric Ackermann <audric@getsession.org>
2025-04-03 19:33:44 +1100

    feat: first release

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,5 @@
 export type LogFn = (...args: Array<unknown>) => void;
 
-export type LoggerType = {
-  fatal: LogFn;
-  error: LogFn;
-  warn: LogFn;
-  info: LogFn;
-  debug: LogFn;
-  trace: LogFn;
-};
-
 // Those are pretty standard and match https://getpino.io/#/docs/api?id=loggerlevels-object
 enum LogLevel {
   Trace = 10,
@@ -19,44 +10,41 @@ enum LogLevel {
   Fatal = 60,
 }
 
-type LogAtLevelFnType = (
-  level: LogLevel,
-  ...args: ReadonlyArray<unknown>
-) => void;
-
 export class Logger {
   private logFn: LogFn | null = null;
 
-  constructor(private label?: string) {}
-
-  setLogFn(fn: LogFn) {
-    this.logFn = fn;
+  constructor(logFn: LogFn) {
+    this.logFn = logFn;
   }
 
   private log(level: LogLevel, ...args: unknown[]) {
     if (!this.logFn) {
       throw new Error('Logger is not initialized: call setLogFn() first.');
     }
-    const prefix = this.label ? `[${this.label}]` : '';
-    this.logFn(level, prefix, ...args);
+    this.logFn(level, ...args);
   }
 
-  trace(...args: unknown[]) {
+  public trace(...args: unknown[]) {
     this.log(LogLevel.Trace, ...args);
   }
-  debug(...args: unknown[]) {
+
+  public debug(...args: unknown[]) {
     this.log(LogLevel.Debug, ...args);
   }
-  info(...args: unknown[]) {
+
+  public info(...args: unknown[]) {
     this.log(LogLevel.Info, ...args);
   }
-  warn(...args: unknown[]) {
+
+  public warn(...args: unknown[]) {
     this.log(LogLevel.Warn, ...args);
   }
-  error(...args: unknown[]) {
+
+  public error(...args: unknown[]) {
     this.log(LogLevel.Error, ...args);
   }
-  fatal(...args: unknown[]) {
+
+  public fatal(...args: unknown[]) {
     this.log(LogLevel.Fatal, ...args);
   }
 }
